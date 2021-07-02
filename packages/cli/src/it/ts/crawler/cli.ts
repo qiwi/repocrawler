33757a12eeb8db5d6839a96d3d82faf7eefdfd5d
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2021-07-02 11:58:50 +0300

    test: add it test for cli

---
+++ packages/cli/src/it/ts/crawler/cli.ts
@@ -0,0 +1,12 @@
+import childProcess from 'child_process'
+import { promisify } from 'util'
+
+const exec = promisify(childProcess.exec)
+
+describe('crawler cli', () => {
+  it ('returns error when no arg is specified', async () => {
+    return expect(exec('yarn crawler')).rejects.toMatchObject({
+      stderr: expect.stringMatching(/^Missing required flag/)
+    })
+  })
+})
