2692a66d96eee7b80032cd66f4f9abd0f6afc8be
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2021-07-02 12:13:31 +0300

    refactor: gerrit

--- packages/gerrit/src/main/ts/crawler.ts
+++ packages/gerrit/src/main/ts/crawler.ts
@@ -1,8 +1,15 @@
 import { GerritKit } from '@gerritkit/rest'
-import { rateLimitWrapper, TCommitInfo, TCrawlerOpts,TGerritkitOpts, TRepoCrawler } from '@qiwi/repocrawler-common'
+import {
+  commonCrawlerMethodsFactory,
+  rateLimitWrapper,
+  TCommitInfo,
+  TCrawlerOpts,
+  TGerritkitOpts,
+  TRepoCrawler,
+  TVcs,
+  writeRepoInfo
+} from '@qiwi/repocrawler-common'
 import { ILogger } from '@qiwi/substrate'
-import { mkdir, promises as fs } from 'fs'
-import { dirname,resolve } from 'path'
 
 const splitRepoName = (data: string[]) =>
   data.map((data) => {
@@ -10,16 +17,18 @@ const splitRepoName = (data: string[]) =>
     return { org, repo }
   })
 
-type getRepos = {
+type TGerritCrawler = {
   getRepos: (orgs?: string[]) => Promise<Array<{ org: string; repo: string }>>
+  gerritkit: InstanceType<typeof GerritKit>
 }
 
 export const createGerritCrawler = (
   { baseUrl, auth }: TGerritkitOpts,
   crawlerOpts: TCrawlerOpts,
   logger: ILogger = console,
-): TRepoCrawler & getRepos => {
+): TRepoCrawler & TGerritCrawler => {
   const gerritkit = rateLimitWrapper(new GerritKit(baseUrl, auth), crawlerOpts.ratelimit)
+  const vcs: TVcs = 'gerrit'
 
   const showLog = crawlerOpts.debug
 
@@ -39,28 +48,18 @@ export const createGerritCrawler = (
     org: string,
     repo: string,
     ref: string,
-  ): Promise<TCommitInfo | undefined> => {
+  ): Promise<TCommitInfo> => {
     return gerritkit.repos
       .getCommit({ owner: org, repo, ref })
-      .then(
-        // @ts-ignore
-        (data) =>
-          ({
-            vcs: 'gerrit',
-            lastCommit: {
-              hash: data.commit,
-              message: data.message,
-              author: data.author,
-              committer: data.committer,
-            },
-          } as TCommitInfo),
-      )
-      .catch((e: Error) => {
-        showLog && logger.warn(
-          `[Gerrit]: Error during getting HEAD commit for ${org}/${repo}: ${e.message}`,
-        )
-        return undefined // eslint-disable-line unicorn/no-useless-undefined
-      })
+      .then((data) => ({
+          vcs: 'gerrit',
+          lastCommit: {
+            hash: data.commit,
+            message: data.message,
+            author: data.author,
+            committer: data.committer,
+          },
+      }))
   }
 
   const getRawContent = async (
@@ -77,94 +76,31 @@ export const createGerritCrawler = (
         ref,
       })
       .then((d: any) => Buffer.from(d, 'base64').toString('utf-8'))
-      .catch((e: Error) => showLog && logger.warn(
-        `[Gerrit]: Error during getting ${path} for ${org}/${repo}: ${e.message}`,
-      ))
   }
 
-  const getContent = async (
-    org: string,
-    repo: string,
-    path: string,
-    ref?: string,
-  ) => {
-    try {
-      const rawContent = await getRawContent(org, repo, path, ref)
-      if (rawContent) {
-        return JSON.parse(rawContent)
-      }
-    } catch (e) {
-      showLog && logger.warn(`[Github]: Error during parsing ${path} for ${org}/${repo}: ${e.message}`)
-    }
-  }
+  const { getRepoFiles, getInfoByRepos, getReportInfoByRepos } = commonCrawlerMethodsFactory(
+    { getCommit, getRawContent },
+    { name: vcs, debug: showLog },
+    logger
+  )
 
-  const getInfoByRepos = async (
-    repos: Array<{ org: string; repo: string }>,
-  ) => {
-    return Promise.all(
-      repos.map(({ org, repo }) => {
-        return Promise.all([
-          getCommit(org, repo, 'HEAD'),
-          getContent(org, repo, 'package.json'),
-          getContent(org, repo, 'package-lock.json'),
-          getRawContent(org, repo, 'yarn.lock'),
-          getContent(org, repo, 'npm-shrinkwrap.json'),
-          getRawContent(org, repo, '.npmrc'),
-          getRawContent(org, repo, 'Makefile'),
-        ]).then(([commitInfo, data, packageLock, yarnLock, shrinkLock, npmrc, makefile]) => {
-          return {
-            name: `gerrit%2F${org}%2F${repo}`,
-            info: commitInfo,
-            package: data,
-            packageLock,
-            yarnLock,
-            shrinkLock,
-            npmrc,
-            makefile,
-          }
-        })
-      }),
-    )
-  }
-
-  const fetchRepoInfo = async (savePath: string, orgs?: Array<string>) => {
+  const fetchRepoInfo: TRepoCrawler['fetchRepoInfo'] = async (savePath, paths, orgs) => {
     const repos = await getRepos()
     const _orgs = orgs
       ? repos.filter(({ org }: { org: string }) => orgs.includes(org))
       : repos
-    const repoInfo = await getInfoByRepos(_orgs)
-
-    try {
-      await Promise.all(
-        repoInfo.map(
-          // @ts-ignore
-          (data: { name: string; info: Record<string, any>; package: any }) => {
-            const resolvedPath = resolve(
-              savePath,
-              `${data.name}${Math.random().toString()}.json`,
-            )
-            return new Promise((resolve) => {
-              mkdir(dirname(resolvedPath), { recursive: true }, () => {
-                fs.writeFile(resolvedPath, JSON.stringify(data)).then(resolve)
-              })
-            })
-          },
-        ),
-      )
-      return true
-    } catch (e) {
-      return false
-    }
+    const repoInfo = await getInfoByRepos(_orgs, paths)
+    return Promise.allSettled(repoInfo.map(data => writeRepoInfo(data, savePath)))
   }
 
   return {
+    getRawContent,
+    getRepoFiles,
     getRepos,
-    // @ts-ignore
     getInfoByRepos,
     fetchRepoInfo,
     getCommit,
-    getContent,
-    // @ts-ignore
     gerritkit,
+    getReportInfoByRepos
   }
 }
--- packages/gerrit/src/test/ts/crawler.ts
+++ packages/gerrit/src/test/ts/crawler.ts
@@ -1,8 +1,13 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
 import { createGerritCrawler } from '../../main/ts'
 import { opts } from '../../main/ts/default'
 import { getCommit } from './stub/getCommit'
 import { getContent } from './stub/getContent'
 import { listProjects } from './stub/listProjects'
+
+const packageExampleJson = readFileSync(join(__dirname, '..', 'resources', 'package.example.json'))
 
 describe('gerritCrowler', () => {
   const gerritkitOpts = {
@@ -68,67 +73,14 @@ describe('gerritCrowler', () => {
     })
   })
 
-  describe('getContent', () => {
+  describe('getRawContent', () => {
     it('returns commit info', async () => {
-      const res = await gerritCrawler.getContent(
+      const res = await gerritCrawler.getRawContent(
         'jslab',
         'repocrawler',
         'package.json',
       )
-      expect(res).toMatchObject({
-        dependencies: {
-          '@octokit/rest': '^18.0.4',
-          '@types/mkdirp': '^1.0.1',
-          axios: '^0.20.0',
-        },
-        description: 'crawler for github and gerrit',
-        devDependencies: {
-          '@qiwi/libdefkit': '^1.0.3',
-          '@swissquote/crafty-preset-jest': '^1.12.0',
-          '@types/jest': '^26.0.13',
-          coveralls: '^3.1.0',
-          'dts-generator': '^3.0.0',
-          eslint: '^7.8.1',
-          'eslint-config-qiwi': '^1.7.0',
-          jest: '^26.4.2',
-          microbundle: '^0.12.3',
-          prettier: '^2.1.1',
-          rimraf: '^3.0.2',
-          terser: '^5.2.1',
-          'ts-jest': '^26.3.0',
-          typedoc: '^0.19.0',
-          'typedoc-plugin-external-module-name': '^4.0.3',
-          typescript: '^4.0.2',
-        },
-        files: ['README.md', 'CHANGELOG.md', 'lib'],
-        main: 'lib/index.js',
-        name: '@qiwi-private/jslab-repocrawler',
-        scripts: {
-          build:
-            'yarn clean && yarn build:es5 && yarn build:es6 && yarn build:ts && yarn build:libdef && yarn docs && yarn uglify && yarn build:bundle',
-          'build:bundle':
-            'microbundle --tsconfig tsconfig.es5.json build src/main/ts/index.ts -o target/bundle',
-          'build:es5': 'mkdirp target/es5 && tsc -p tsconfig.es5.json',
-          'build:es6': 'mkdirp target/es6 && tsc -p tsconfig.es6.json',
-          'build:libdef':
-            'dts-generator --project ./ --out typings/index.d.ts --prefix @qiwi-private/jslab-repocrawler/target/es5 --name @qiwi-private/jslab-repocrawler --main @qiwi-private/jslab-repocrawler/target/es5/index --moduleResolution node && libdeffix --dts=./typings/index.d.ts --prefix=@qiwi-private/jslab-repocrawler/target/es5 && flowgen typings/index.d.ts --output-file flow-typed/index.flow.js',
-          'build:ts': 'cp -r src/main/ts/ target/ts/',
-          clean: 'rimraf target typings flow-typed buildcache',
-          docs:
-            'typedoc --readme README.md --tsconfig tsconfig.json src/main --ignoreCompilerErrors || exit 0',
-          format: 'prettier --write "src/**/*.ts"',
-          jest: 'jest -w 1 --config=jest.config.json',
-          lint: 'eslint src/**/*.ts',
-          postupdate: 'yarn && npx yarn-audit-fix && yarn build && yarn test',
-          'push:report': 'yarn coveralls:push',
-          test: 'yarn lint && yarn jest',
-          'test:report': 'yarn test && yarn push:report',
-          uglify:
-            // eslint-disable-next-line no-template-curly-in-string
-            "for f in $(find target -name '*.js'); do short=${f%.js}; terser -c -m -o $short.js -- $f; done",
-        },
-        version: '0.0.1',
-      })
+      expect(res).toEqual(packageExampleJson.toString())
     })
   })
 })
